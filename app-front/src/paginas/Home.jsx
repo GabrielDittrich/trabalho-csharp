7ce1ad007f884505e4d851a1e759ffa4cf0e1713
Gabriel Dittrich <gdittrchcj@gmail.com>
2024-10-15 14:54:32 -0300

    Update Home.jsx

--- app-front/src/paginas/Home.jsx
+++ app-front/src/paginas/Home.jsx
@@ -4,7 +4,7 @@ import MinhaImagem from '../imagens/golden-retriever.jpg'; // Importe a imagem
 function Home() {
     return (
         <div className="principal">
-            <h3>Seja Bem-Vindo Ao PetShop</h3>
+            <h3>Bem-Vindo Ao PetShop</h3>
             <img src={MinhaImagem} alt="Imagem de Boas-Vindas" /> {/* Use a variável que armazena o caminho da imagem */}
         </div>
     );
